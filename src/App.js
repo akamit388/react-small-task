96869296a996b88dff24d0660f4a238a83eed6a8
Amit Kumar <akamit388@gmail.com>
2023-01-17 00:26:03 +0530

    new repo

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component, useState, useEffect } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import LifeCycleUseEffect from './components/LifeCycleUseEffect';
+// import TopDiv from './components/getColor/TopDiv';
+// import Task1 from './components/Task1';
+// import Task2 from './components/Task2';
+// import Task3 from './components/Task3';
+// import DataShort from './components/DataShort';
+// import DataShort1 from './components/DataShort1';
+//import SelectBoxCheck from './components/SelectBoxCheck';
+// import SelectBoxValue from './components/SelectBoxValue';
+// import ComOne from './components/contextapi/ComOne';
+// import CheckboxDelete from './components/CheckboxDelete';
+// import CheckboxDeleteTwo from './components/CheckboxDeleteTwo';
+// import ComOne from './components/hooks/usecontest/ComOne';
+
+
+const App = () => {
+
+  return (
+    <>
+      {/* <Task1/> */}
+      {/* <Task2/> */}
+      {/* <Task3/> */}
+      {/* <DataShort/> */}
+      {/* <DataShort1/> */}
+      {/* <SelectBoxCheck/> */}
+      {/* <SelectBoxValue/> */}
+      {/* <ComOne/> */}
+      {/* <CheckboxDelete/> */}
+      {/* <CheckboxDeleteTwo/> */}
+      {/* <ComOne/> */}
+      {/* <TopDiv/> */}
+      <LifeCycleUseEffect/>
+    </>
+  )
+}
+
+export default App
